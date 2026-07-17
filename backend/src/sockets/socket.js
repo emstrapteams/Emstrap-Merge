7ba1354d0200405e3b591c7afaafce2fc0ffaf03
mohithraj0731 <rajmohith23@gmail.com>
2026-07-17 18:09:13 +0530

    Use Render backend in production

--- backend/src/sockets/socket.js
+++ backend/src/sockets/socket.js
@@ -9,6 +9,7 @@ const allowedOrigins = [
   "http://localhost:5174",
   "http://127.0.0.1:5174",
   "https://emstrapteams.netlify.app",
+  "https://emstrap.com",
   "https://emstrap-finalworking.netlify.app",
   "https://emstrap-merge.netlify.app",
 ].filter(Boolean);
